8c4e88bb98d517d23fe19826c8c580f93ac4e3e7
Binaya shrestha <sthavicky555@gmail.com>
2025-07-20 20:04:52 +0545

    analysis system

---
+++ Tracker/tracker.py
@@ -0,0 +1,167 @@
+from ultralytics import YOLO
+import supervision as sv
+import pickle
+import cv2 as cv
+import os
+import numpy as np
+import pandas as pd
+import sys
+sys.path.append('../')
+from utils import get_center_of_bbox, get_bbox_width
+
+class Tracker:
+    def __init__(self, model_path):
+        self.model = YOLO(model_path)
+        self.tracker = sv.ByteTrack()
+   
+    def detector(self, frames):
+        #detect frames in batch size of 20 at a time
+        batch_size = 20
+        detections = []
+        for i in range(0, len(frames), batch_size):
+            detection_batch = self.model.predict(frames[i:i+batch_size], conf=0.1)
+            detections += detection_batch
+        return detections
+    
+    #function to track the players, referees and ball in each frame of video
+    def get_tracks(self, frames, read_from_stub = False, stub_path = None):
+        #load pickle file
+        if read_from_stub and stub_path is not None and os.path.exists(stub_path):
+            with open(stub_path, 'rb') as f:
+                tracks = pickle.load(f)
+            return tracks
+
+        detections = self.detector(frames)
+        tracks = {
+            'player': [],
+            'referee': [],
+            'ball': []
+        }
+
+        for frame_num, detection in enumerate(detections):
+            cls_names = detection.names
+            cls_names_inv = {v:k for k,v in cls_names.items()}
+
+            #convert to supervision detection format
+            detection_supervision = sv.Detections.from_ultralytics(detection)
+
+            #convert goalkeeper to player
+            for id_num, cls_id in enumerate(detection_supervision.class_id):
+                if cls_names[cls_id] == 'goalkeeper':
+                    detection_supervision.class_id[id_num] = cls_names_inv['player']
+
+            tracks_supervision = self.tracker.update_with_detections(detection_supervision)
+
+            tracks['player'].append({})
+            tracks['referee'].append({})
+            tracks['ball'].append({})
+
+            #for player and referee
+            for frame_detection in tracks_supervision:
+                bbox = frame_detection[0].tolist()
+                cls_id = frame_detection[3]
+                track_id = frame_detection[4]
+
+                if cls_id == cls_names_inv['player']:
+                    tracks['player'][frame_num][track_id] = {"bbox": bbox}
+
+                if cls_id == cls_names_inv['referee']:
+                    tracks['referee'][frame_num][track_id] = {"bbox": bbox}
+
+            #for ball
+            for frame_detection in detection_supervision:
+                bbox = frame_detection[0].tolist()
+                cls_id = frame_detection[3]
+
+                if cls_id == cls_names_inv['ball']:
+                    tracks['ball'][frame_num][1] = {"bbox": bbox}
+
+        #create pickle file for future use
+        if stub_path is not None:
+            with open(stub_path, 'wb') as f:
+                pickle.dump(tracks, f)
+        
+        return tracks
+    
+    def draw_ellipse(self, frame, bbox, color, track_id=None):
+        y2 = int(bbox[3])
+        center_x, _ = get_center_of_bbox(bbox)
+        width = get_bbox_width(bbox)
+        cv.ellipse(frame, 
+                   center=(center_x, y2),
+                   axes=(int(width), int(0.35*width)),
+                   angle=0,
+                   startAngle=-45,
+                   endAngle=235,
+                   color= color,
+                   thickness=2,
+                   lineType= cv.LINE_4
+                   )
+        
+        #draw rectangle for the track id
+        rectangle_width = 40
+        rectangle_height=20
+        x1_rect = center_x - rectangle_width//2
+        x2_rect = center_x + rectangle_width//2
+        y1_rect = (y2 - rectangle_height//2) +15
+        y2_rect = (y2 + rectangle_height//2) +15
+
+        if track_id is not None:
+            cv.rectangle(frame,
+                          (int(x1_rect),int(y1_rect) ),
+                          (int(x2_rect),int(y2_rect)),
+                          color,
+                          cv.FILLED)
+        
+            x1_text = x1_rect+12
+            if track_id > 99:
+                x1_text -=10
+            
+            cv.putText(frame,
+                f"{track_id}",
+                (int(x1_text),int(y1_rect+15)),
+                cv.FONT_HERSHEY_SIMPLEX,
+                0.6,
+                (0,0,0),
+                2
+            )
+        return frame
+    
+    def draw_traingle(self,frame,bbox,color):
+        y= int(bbox[1])
+        x,_ = get_center_of_bbox(bbox)
+
+        triangle_points = np.array([
+            [x,y],
+            [x-10,y-20],
+            [x+10,y-20],
+        ])
+        cv.drawContours(frame, [triangle_points],0,color, cv.FILLED)
+        cv.drawContours(frame, [triangle_points],0,(0,0,0), 2)
+
+        return frame
+    
+    def draw_annotation(self, frames, tracks):
+        output_video_frame = []
+        for frame_num, frame in enumerate(frames):
+            frame = frame.copy()
+
+            player_dict = tracks['player'][frame_num]
+            referee_dict = tracks['referee'][frame_num]
+            ball_dict = tracks['ball'][frame_num]
+
+            #draw player
+            for track_id, player in player_dict.items():
+                frame = self.draw_ellipse(frame, player['bbox'], (0,0,255), track_id)
+            
+            #draw referee
+            for track_id, referee in referee_dict.items():
+                 frame = self.draw_ellipse(frame, referee['bbox'], (0,255,255))
+                
+            #draw ball
+            for track_id, ball in ball_dict.items():
+                 frame = self.draw_traingle(frame, ball['bbox'], (0,255,255))
+
+            output_video_frame.append(frame)
+        
+        return output_video_frame
--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import cv2 as cv
 from ultralytics import YOLO
 from utils import read_video, save_video
+from Tracker import Tracker
 
 def main():
 
     model = YOLO("Football_analysis_system\Model\\best.pt")
-    video_frames = read_video("Football_analysis_system\Videos\\2_cut.mp4")
-    for f in video_frames:
-        frame = model(f, stream=True)
-        
+    video_frames = read_video("Football_analysis_system\Videos\\1.mp4")
+    
+    tracker_obj = Tracker("Football_analysis_system\Model\\best.pt")
+    tracks = tracker_obj.get_tracks(video_frames,
+                                    read_from_stub=True,
+                                    stub_path="Football_analysis_system\Stubs\\track_stub.pkl")
 
-    save_video(video_frames, "Football_analysis_system\Output\output2.avi")
+    output_video_frames = tracker_obj.draw_annotation(video_frames, tracks)
+    #save video
+    save_video(output_video_frames, "Football_analysis_system\Output\output2.avi")
 
 if __name__ == "__main__":
     main()
